3948625cf3ef72bad9f7cdfabe5e584c21c59afd
Galio <yinmobai@gmail.com>
2018-08-29 18:55:23 +0800

    first commit

---
+++ .vuepress/config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  title: "Yinmobai's blog", // 标题
+  description: "个人博客，文档分享", // 描述
+  head: [["link", { rel: "icon", href: `/new2.png` }]], // 被注入页面 HTML <head> 额外的标签
+  host: "0.0.0.0", // 访问路径
+  port: "5555", // 端口
+  dest: ".vuepress/dist" // 输出目录
+};
